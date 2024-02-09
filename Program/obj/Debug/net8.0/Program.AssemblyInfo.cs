44df09c53293c75fdb5f96fc26b6881e76e8eb08
royhalesx <Royhalesx@gmail.com>
2024-02-09 15:57:49 -0700

    MinorChanges

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7630bdf9a572718777f8be9c83e6eed1e5605c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2822be9ce5a99ce32fa95a1b06c807ac241262e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
